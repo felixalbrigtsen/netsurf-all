6af6d27f4c1716490bf68fd1db648f0a6cc92fdd
Felix Albrigtsen <felix@albrigtsen.it>
2024-03-14 11:29:33 +0100

    add pwndbg wrapper

--- flake.nix
+++ flake.nix
@@ -18,6 +18,16 @@
       pkgs = import nixpkgs { inherit system; };
       pkgs-unstable = import unstable { inherit system; };
 
+      pwndbgWrapped = pkgs.stdenvNoCC.mkDerivation {
+        name = "pwndbgWrapped";
+        phases = [ "installPhase" ];
+        # ln is broken, see https://github.com/NixOS/nix/issues/9579
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ${pkgs.pwndbg}/bin/pwndbg $out/bin/gdb
+        '';
+      };
+
       default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           zsh
@@ -27,7 +37,7 @@
 
           (lib.recurseIntoAttrs (callPackage ./netsurf-nix { })).browser
         ] ++ lib.optionals stdenv.isLinux [
-          pwndbg
+          pwndbgWrapped
         ];
 
         shellHook = ''
